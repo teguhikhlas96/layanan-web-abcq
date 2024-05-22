e7d182c2dad219b01687ea61c33ad538a9908db1
Teguh Ikhlas Ramadhan <gamanspin48@gmail.com>
2024-05-22 10:16:49 +0700

    menambahkan tampilan login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('login', 'Auth\LoginController@login');
+
+
+require __DIR__.'/auth.php';
